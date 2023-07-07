4b513e05b78e421bfc42ffe013455b28eebbdb0b
linliu7 <linliu7@publicisgroupe.net>
2023-07-07 13:50:48 -0400

    login screen now has theme colors

--- src/pages/unauthenticatedHomepage.tsx
+++ src/pages/unauthenticatedHomepage.tsx
@@ -1,14 +1,14 @@
 export default function UnauthenticatedHomepage() {
 	return (
 		<>
-			<div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-6xl font-bold">Fortuna</h1>
-      <p className="py-6 text-3xl">Get better results with Fortuna at the helm of your portfolio</p>
+			<div className="hero min-h-[85vh] bg-primary">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className="text-center lg:text-left p-10">
+      <h1 className="text-8xl font-bold text-secondary">Fortuna</h1>
+      <p className="py-6 text-3xl text-secondary">Get better results with Fortuna at the helm of your portfolio</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <div className="card-body bg-secondary">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
@@ -28,22 +28,21 @@ export default function UnauthenticatedHomepage() {
 		<div>
 			<hr
 			style={{
-            color: "red",
-            backgroundColor: "red",
-            height: 1
+        borderColor: "#ababab",
 		}}
 		/>
 		</div>
 		<div className="label-text-alt">Don't have an account? 
-			<a className="link link-hover"> Sign up</a>
+			<a className="link link-hover "> Sign up</a>
 		</div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Sign in</button>
+          <button className="btn btn-neutral text-white">Sign in</button>
         </div>
       </div>
     </div>
   </div>
 </div>
+<div className="h-[15vh] bg-gradient-to-b from-[#707099] to-[#f0ecd8]}"></div>
 		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	mode: 'jit',
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
@@ -7,9 +8,17 @@ export default {
 	plugins: [require("daisyui")],
 
 	daisyui: {
-		theme: [
+		themes: [
 			{
-				
+			mytheme: {
+				"primary": "#707099",
+				"secondary": "#f0ecd8",
+				"accent":"#dfbea5",
+				"neutral": "#adaa9b",
+				"base-100": "white",
+				"success": "#e5f9e7",
+				"success-content": "#c4e4bc",
+			}	
 			}
 		]
 	}
