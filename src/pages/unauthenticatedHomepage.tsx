decae70fd605b2ef8112269a591ab6f22bfe99f9
linliu7 <linliu7@publicisgroupe.net>
2023-07-10 13:24:49 -0400

    unauthenticatedHomepage is done

--- src/pages/unauthenticatedHomepage.tsx
+++ src/pages/unauthenticatedHomepage.tsx
@@ -1,3 +1,7 @@
+import {BsStopwatch} from 'react-icons/bs'
+import {LiaHandshakeSolid} from 'react-icons/lia'
+import {BiPopsicle} from 'react-icons/bi'
+
 export default function UnauthenticatedHomepage() {
 	return (
 		<>
@@ -42,8 +46,28 @@ export default function UnauthenticatedHomepage() {
     </div>
   </div>
 </div>
-<div className="h-[15vh] bg-gradient-to-b from-[#707099] to-[#f0ecd8]}"></div>
-		</>
+<div className="h-[15vh] bg-gradient-to-b from-[#707099] to-[#f0ecd8]"></div>
+
+
+
+<div className="flex flex-row w-full bg-[#f0ecd8] text-left ">
+  <div className="grid flex-grow card rounded-box max-w-[60%]  p-7">
+    <p className="text-4xl font-bold p-1">We love to trade, and we hope you do too!</p>
+    <p className="text-xl p-1">Now you can express that love with the latest trading platform from Fortuna. We guarantee a 0-second trade execution, and will give you a free popsicle if your trade doesn’t go through in this time period</p>
+    <p className="text-xl p-1">On a side-note, we just bought a big freezer and 10,000 popsicles. Thankfully, we’ve hedged all popsicle expenses for the next two years! (Commodity experts say popsicles are on the rise)</p>
+  </div> 
+  <div className="inline-block h-[250px] min-h-[1em] w-0.5 self-center bg-neutral"></div>
+  <div className="grid flex-grow h-[40vh] card rounded-box max-w-[40%] p-7 ml-9">
+    <p className = "text-4xl font-bold sm: mb-5">Every account comes with:</p>
+    <ul className='text-xl flex flex-col lg: gap-8'>
+      <li className="inline-flex items-center"><BsStopwatch className="mr-4"/>0-second execution</li>
+      <li className="inline-flex items-center"><LiaHandshakeSolid className="mr-4"/>Portfolio insurance</li>
+      <li className="inline-flex items-center"><BiPopsicle className= "mr-4"/>1 popsicle</li>
+    </ul>
+    </div>
+</div>
+<div className="h-[15vh] bg-gradient-to-b from-[#f0ecd8] to-white"></div>
+    </>
 	);
 }
 
