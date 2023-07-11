a8c34bbc827de0075b65814476b7ecfc6a77bf44
linliu7 <linliu7@publicisgroupe.net>
2023-07-11 14:56:20 -0400

    pushing buy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
 } from "react-router-dom";
 import ApplicationLayout from "./components/layout/application";
-import UnauthenticatedHomepage from "./pages/unauthenticatedHomepage";
-// import Buy from "./pages/Buy";
+import Buy from "./pages/Buy";
 import Overview from "./pages/application views/overview";
 import Holdings from "./pages/application views/holdings";
-<<<<<<< HEAD
-import NoMatch from "./components/noMatch";
 import UnauthenticatedHomepage from "./pages/unauthenticatedHomepage";
+import NoMatch from "./components/utility/noMatch";
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
         <Route index element={<UnauthenticatedHomepage />} />
         <Route path="/buy" element={<Buy />} />
         <Route path="/app">
-          <Route path=":userId" element={<ApplicationLayout />} />
+          <Route path=":userId" element={<ApplicationLayout />}>
             <Route path="overview" element={<Overview />} />
             <Route path="holdings" element={<Holdings />} />
             <Route path="*" element={<NoMatch />} />
@@ -29,27 +27,6 @@ function App() {
       </Routes>
     </Router>
   );
-=======
-import NoMatch from "./components/utility/noMatch";
-
-function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route index path="/" element={<UnauthenticatedHomepage />} />
-				<Route path="/app">
-					<Route index element={<Navigate to="/" replace />} />
-					<Route path=":userId" element={<ApplicationLayout />}>
-						<Route index element={<Navigate to="overview" replace />} />
-						<Route path="overview" element={<Overview />} />
-						<Route path="holdings" element={<Holdings />} />
-						<Route path="*" element={<NoMatch />} />
-					</Route>
-				</Route>
-			</Routes>
-		</Router>
-	);
->>>>>>> e492fa08cdf0630e4de8419c62a74a902619388b
 }
 
 export default App;
