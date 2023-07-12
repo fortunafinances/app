427f3f317d06743b64102238de2f2aa701a56355
linliu7 <linliu7@publicisgroupe.net>
2023-07-12 11:14:24 -0400

    created buySellSymbolQuantityLimit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Select from "react-select";
 import App from "./App.tsx";
 import "../index.css";
 
--- src/pages/Buy.tsx
+++ src/pages/Buy.tsx
@@ -1,19 +1,6 @@
-import Select from "react-select";
-import { useState } from "react";
-import { BiDollar } from "react-icons/bi";
+import SymbolQuantityLimit from "../components/input/buySellSymbolQuantityLimit";
 
 export default function Buy() {
-  const aquaticCreatures = [
-    { label: "Shark", value: "Shark" },
-    { label: "Dolphin", value: "Dolphin" },
-    { label: "Whale", value: "Whale" },
-    { label: "Octopus", value: "Octopus" },
-    { label: "Crab", value: "Crab" },
-    { label: "Lobster", value: "Lobster" },
-  ];
-
-  const [marketState, setMarketState] = useState(true);
-
   return (
     <div className="h-screen flex">
       <div className="bg-success w-[35%]">
@@ -21,79 +8,8 @@ export default function Buy() {
           <h1 className="text-4xl">BUY</h1>
           <p className="text-xl">Account Name</p>
         </div>
-        <div className="m-4 mt-6 flex flex-col gap-3">
-          <h1 className="font-semibold text-xl">Symbol</h1>
-          <Select
-            options={aquaticCreatures}
-            theme={(theme) => ({
-              ...theme,
-              borderRadius: 3,
-            })}
-          />
-        </div>
-        <div className="m-4 mt-6 flex flex-col gap-3">
-          <h1 className="font-semibold text-xl">Quantity</h1>
-          {/* <div className="border-[1px] rounded-[3px] border-[#cccccc] "> */}
-          <input
-            type="number"
-            min={1}
-            step={1}
-            placeholder="Stock Amount"
-            className="input rounded-sm h-9 w-full border-[1px] rounded-[3px] border-[#cccccc] focus:ring-blue-500 focus:border-blue-500 focus:border-[2px] !outline-none"
-          />
-          {/* </div> */}
-        </div>
-        <div className="flex flex-row justify-evenly font-semibold [&>button]:w-full px-4 h-10 [&>button]:border border-[#cccccc] rounded-sm">
-          <button
-            onClick={() => {
-              setMarketState(true);
-            }}
-            className={
-              marketState ? "bg-[#e6e6e6] shadow-lg shadow-inner" : "bg-white"
-            }
-          >
-            Market
-          </button>
-          <button
-            onClick={() => {
-              setMarketState(false);
-            }}
-            className={
-              !marketState ? "bg-[#e6e6e6] shadow-lg shadow-inner" : "bg-white"
-            }
-          >
-            Limit
-          </button>
-        </div>
-        {!marketState ? (
-          <span className="m-4 mt-6 flex flex-col gap-3">
-            <h1 className="font-semibold text-xl">Limit Price</h1>
-            <div className="relative">
-              <i className="absolute top-[50%] -translate-y-[50%] align-middle">
-                <BiDollar />
-              </i>
-              <input
-                type="number"
-                min={0}
-                step="0.01"
-                placeholder="Price"
-                className="input rounded-sm h-9 w-full border-[1px] rounded-[3px] border-[#cccccc] focus:ring-blue-500 focus:border-blue-500 focus:border-[2px] !outline-none"
-              />
-            </div>
-          </span>
-        ) : null}
-        <div className="m-4 mt-6 flex flex-row gap-3 font-semibold text-xl">
-          <h1>Total Price</h1>
-          <h1>$1234</h1>
-        </div>
-        <div className="flex flex-row justify-end m-4 gap-4 text-xl [&>button]:border [&>button]:rounded-xl [&>button]:px-3 [&>button]:py-1 [&>button]:border-4 [&>button]:font-bold">
-          <button className="border-[#920000] text-[#920000] bg-[#F9E5E5] hover:shadow-xl shadow-[#920000]">
-            Cancel
-          </button>
-          <button className="border-success-content text-success-content bg-[#E3FDDC] hover:shadow-xl shadow-succes-content">
-            Submit
-          </button>
-        </div>
+        <SymbolQuantityLimit />
+        np
       </div>
       <div className="flex-1 flex overflow-hidden">
         <div className="flex-1 overflow-y-scroll">
