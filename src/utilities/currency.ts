fd485fad3e36d85a86ffb33b2f58ff186f2ecb42
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-07-18 12:38:26 -0400

    Fix currency filter min value

--- src/utilities/currency.ts
+++ src/utilities/currency.ts
@@ -12,7 +12,7 @@ export function filterRange(
 	_columnIds: string,
 	filterValue: number[]
 ): boolean {
-	const min = filterValue[0] ? filterValue[0] * 100 : 0;
+	const min = filterValue[0] ? filterValue[0] * 100 : Number.NEGATIVE_INFINITY;
 	const max = filterValue[1] ? filterValue[1] * 100 : Number.POSITIVE_INFINITY;
 	return fieldValue >= min && fieldValue <= max;
 }
