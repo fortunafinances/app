43fb95d4ca0514a6ac6fedfd8c4943af7d08dbf0
linliu7 <linliu7@publicisgroupe.net>
2023-07-19 11:42:27 -0400

    added symbol to reactive var

--- src/utilities/reactiveVariables.ts
+++ src/utilities/reactiveVariables.ts
@@ -1,20 +1,23 @@
+import { makeVar } from "@apollo/client/cache/inmemory/reactiveVars";
 import { makeVarPersisted } from "./hooks";
 
 export const sidebarClosed = makeVarPersisted("sidebarClosed", false);
 export const currentAccountId = makeVarPersisted("currentAccountId", 1);
 export const userId = makeVarPersisted("user", {
-	userId: 1,
-	username: "Nolan Gelinas",
+  userId: 1,
+  username: "Nolan Gelinas",
 });
 
+export const symbol = makeVar("");
+
 export const accounts = [
-	{
-		id: 1,
-		name: "Brokerage",
-	},
-	{
-		id: 2,
-		name: "College Fund",
-	},
-	{ id: 3, name: "Extremely Super Duper Long-Named Fund" },
+  {
+    id: 1,
+    name: "Brokerage",
+  },
+  {
+    id: 2,
+    name: "College Fund",
+  },
+  { id: 3, name: "Extremely Super Duper Long-Named Fund" },
 ];
