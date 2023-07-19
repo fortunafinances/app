08ab6415f317a67315cca26a58adf60202e0c6fe
LV <linvu2@publicisgroupe.net>
2023-07-19 16:29:15 -0400

    removed the extra auth config

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,6 @@ import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Auth0Provider
-			domain="dev-wpc8kymxzmepqxl5.us.auth0.com"
-			clientId="OxQxuofsPZXSFzTqbVtKgErT2xrl3VfZ"
-			authorizationParams={{
-				redirect_uri: "http://localhost:4040/callback",
-			}}
-		>
-			<App />
-		</Auth0Provider>
+		<App />
 	</React.StrictMode>
 );
