20c9437fbfeaac3780354843e7ddf3616df4f133
Nolan Gelinas <nolangelinas@gmail.com>
2023-07-20 11:06:37 -0400

    Fix price name and add autosizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Auth0Provider } from "@auth0/auth0-react";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
