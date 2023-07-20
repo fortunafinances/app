6dcdae03b3bb9f551c54010b61daea8f5b5d7b8f
lindsayliu <rll46@duke.edu>
2023-07-20 13:36:21 -0400

    added color to transfer successful

--- src/components/popup/transferSuccessful.tsx
+++ src/components/popup/transferSuccessful.tsx
@@ -14,18 +14,20 @@ export default function TransferSuccessful() {
   return (
     <div className="h-screen flex items-center justify-center">
       {showAlert ? (
-        <div className="alert shadow-lg max-w-fit min-h-[25%] bg-secondary">
+        <div className="alert flex flex-auto shadow-lg max-w-fit min-h-[25%] min-w-fit px-10 bg-[#f2fcef]">
           <div className="flex flex-col gap-6 p-3 text-primary items-center">
             <h3 className="font-bold text-4xl">SUCCESSFUL</h3>
             <div className="text-xl">{message}</div>
             <div className="flex flex-row gap-3 [&>button]:text-primary [&>button]:border-primary">
               <button
-                className="btn btn-sm"
+                className="btn btn-sm bg-white hover:bg-primary hover:text-white"
                 onClick={() => setShowAlert(false)}
               >
                 Close
               </button>
-              <button className="btn btn-sm">{buttonName}</button>
+              <button className="btn btn-sm bg-white hover:bg-primary hover:text-white">
+                {buttonName}
+              </button>
             </div>
           </div>
         </div>
