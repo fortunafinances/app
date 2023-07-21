6730e54d2ac8217e1bb28d06b6f341ac6a464c6b
lindsayliu <rll46@duke.edu>
2023-07-21 15:02:07 -0400

    createProfile layout done

--- src/pages/application views/createProfile.tsx
+++ src/pages/application views/createProfile.tsx
@@ -1,17 +1,18 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function CreateProfile() {
   return (
     <div className="h-screen flex [&>div]:w-[50%]">
       <div className="flex flex-col gap-5 bg-primary text-secondary p-8">
-        <h1 className=" mt-[50%] font-semibold text-left md:text-8xl text-5xl">
+        <h1 className=" mt-[30%] font-semibold text-left md:text-8xl text-5xl">
           Welcome to Fortuna
         </h1>
         <h2 className=" inline-block text-4xl">
           Get better results with{" "}
-          <h3 className="font-bold text-left inline-block text-black bg-secondary px-2">
+          <p className="font-bold text-left inline-block text-black bg-secondary px-2">
             Fortuna
-          </h3>{" "}
+          </p>{" "}
           at the helm of your portfolio
         </h2>
       </div>
@@ -43,15 +44,15 @@ export default function CreateProfile() {
                   birthdate: "",
                 };
                 if (!values.firstname) {
-                  errors.firstname = "Required";
+                  errors.firstname = "*Required";
                 }
 
                 if (!values.lastname) {
-                  errors.lastname = "Required";
+                  errors.lastname = "*Required";
                 }
 
                 if (!values.email) {
-                  errors.email = "Required";
+                  errors.email = "*Required";
                 } else if (
                   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                 ) {
@@ -59,61 +60,92 @@ export default function CreateProfile() {
                 }
 
                 if (!values.phonenumber) {
-                  errors.phonenumber = "Required";
+                  errors.phonenumber = "*Required";
                 } else if (
-                  !/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/i.test(
+                  !/^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/i.test(
                     values.phonenumber
                   )
                 ) {
                   errors.phonenumber = "Invalid phone number";
                 }
                 if (!values.birthdate) {
-                  errors.birthdate = "Required";
+                  errors.birthdate = "*Required";
                 }
                 return errors;
               }}
             >
               {({ isSubmitting }) => (
-                <Form className="flex flex-col gap-4">
+                <Form className="flex flex-col gap-4 ">
                   <div>
+                    <h1 className="text-left text-3xl font-medium pl-1">
+                      First Name
+                    </h1>
+                    <ErrorMessage
+                      name="firstname"
+                      component="div"
+                      className="text-left text-[#FF0000]"
+                    />
                     <Field
                       type="text"
                       name="firstname"
                       placeholder="First name"
-                      className="h-10"
+                      className="pl-3 h-14 w-full rounded-md text-xl"
                     />
-                    <ErrorMessage name="firstname" component="div" />
                   </div>
                   <div>
+                    <h1 className="text-left text-3xl font-medium pl-1">
+                      Last Name
+                    </h1>
+                    <ErrorMessage
+                      name="lastname"
+                      component="div"
+                      className="text-left text-[#FF0000]"
+                    />
                     <Field
                       type="text"
                       name="lastname"
                       placeholder="Last name"
-                      className="h-10"
+                      className="pl-3 h-14 w-full rounded-md text-xl"
                     />
-                    <ErrorMessage name="lastname" component="div" />
                   </div>
                   <div>
+                    <h1 className="text-left text-3xl font-medium pl-1">
+                      Email
+                    </h1>
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-left text-[#FF0000]"
+                    />
                     <Field
                       type="email"
                       name="email"
                       placeholder="Email Address"
-                      className="h-10"
+                      className="pl-3 h-14 w-full rounded-md text-xl"
                     />
-                    <ErrorMessage name="email" component="div" />
                   </div>
                   <div>
+                    <h1 className="text-left text-3xl font-medium pl-1">
+                      Phone Number
+                    </h1>
+                    <ErrorMessage
+                      name="phonenumber"
+                      component="div"
+                      className="text-left text-[#FF0000]"
+                    />
                     <Field
                       type="phonenumber"
                       name="phonenumber"
                       placeholder="Phone number"
-                      className="h-10"
+                      className="pl-3 h-14 w-full rounded-md text-xl"
                     />
-                    <ErrorMessage name="phonenumber" component="div" />
                   </div>
-
-                  <button type="submit" disabled={isSubmitting}>
-                    Submit
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="flex flex-row justify-end"
+                  >
+                    <BsArrowRight size={60} />
                   </button>
                 </Form>
               )}
