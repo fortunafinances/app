44aeee2ed1fc079fbfe806e8eed88309d36de3fe
LV <linvu2@publicisgroupe.net>
2023-07-24 14:25:44 -0400

    added buttons behavior

--- src/components/data/lineChart.tsx
+++ src/components/data/lineChart.tsx
@@ -9,7 +9,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { lineChartDateRange } from '../../utilities/reactiveVariables';
+import { dateRanges, lineChartDateRange } from '../../utilities/reactiveVariables';
 import { useReactiveVar } from "@apollo/client/react/hooks/useReactiveVar";
 
 ChartJS.register(
@@ -36,34 +36,33 @@ export function LineChart() {
         },
     };
 
-    // initalize date range
-    lineChartDateRange([
-        {
-            full: [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December"
-            ],
-            half: ["January", "February", "March", "April", "May", "June"],
-            quarter: ["January", "February", "March"]
-        }
-    ]);
+    // create range var to store the selection
+    // lineChartDateRange([
+    //     {
+    //         full: [
+    //             "January",
+    //             "February",
+    //             "March",
+    //             "April",
+    //             "May",
+    //             "June",
+    //             "July",
+    //             "August",
+    //             "September",
+    //             "October",
+    //             "November",
+    //             "December"
+    //         ],
+    //         half: ["January", "February", "March", "April", "May", "June"],
+    //         quarter: ["January", "February", "March"]
+    //     }
+    // ]);
     const range = useReactiveVar(lineChartDateRange)
-    const rangeLable = range[0].full;
-    console.log("range", rangeLable)
-
+   
+    console.log("range", range)
 
     const data = {
-        labels: rangeLable,
+        labels: range!,
         datasets: [
             {
                 label: "Dataset 1",
@@ -84,13 +83,19 @@ export function LineChart() {
         <div>
             <Line options={chart} data={data} />
             <div className="inline-flex">
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4 rounded-l">
+                <button
+                    onClick={() => lineChartDateRange(dateRanges.quarter)}
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4 rounded-l">
                     3 Months
                 </button>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4">
+                <button 
+                    onClick={() => lineChartDateRange(dateRanges.half)}
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4">
                     6 Months
                 </button>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4 rounded-r">
+                <button
+                    onClick={() => lineChartDateRange(dateRanges.full)} 
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold border border-gray-400 py-2 px-4 rounded-r">
                     12 Months
                 </button>
             </div>
