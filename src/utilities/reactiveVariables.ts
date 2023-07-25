078d7a4975cc71de53ea26aeb5ab30b8b66245c5
LV <linvu2@publicisgroupe.net>
2023-07-25 10:40:28 -0400

    changed quarter to 24 months

--- src/utilities/reactiveVariables.ts
+++ src/utilities/reactiveVariables.ts
@@ -23,9 +23,9 @@ export const userInfo = makeVarPersisted<User | null>("user", {
 });
 
 export const dateRanges = {
+  two: getMostRecentMonths(24),
   full: getMostRecentMonths(12),
   half: getMostRecentMonths(6),
-  quarter: getMostRecentMonths(3),
 }
 
 export const lineChartDateRange = makeVarPersisted<string[] | null>("lineChartDateRange",
