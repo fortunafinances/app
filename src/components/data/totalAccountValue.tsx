242191c20282fcb56215c8b68258063e3e3fb193
Nolan Gelinas <nolangelinas@gmail.com>
2023-07-26 15:25:12 -0400

    Connect total account value

--- src/components/data/totalAccountValue.tsx
+++ src/components/data/totalAccountValue.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 import { formatDollars } from "../../utilities/currency";
 import { formatDate } from "../../utilities/common";
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { userInfo } from "../../utilities/reactiveVariables";
+
+const GET_TOTAL_VALUE = gql`
+	query AllAccValue($userId: String!) {
+		allAccValue(input: { userId: $userId })
+	}
+`;
 
 export default function TotalAccountValue() {
-	const mockData = 1000000;
 	const [date] = useState(new Date());
+
+	const user = useReactiveVar(userInfo);
+
+	const { loading, data } = useQuery<{ allAccValue: number }>(GET_TOTAL_VALUE, {
+		variables: { userId: user?.userId },
+	});
+
 	return (
 		<div className="bg-yellow-400 w-full flex flex-col gap-1 p-2 text-black items-center">
 			<p>All Accounts</p>
-			<h3 className="text-xl font-bold">{formatDollars(mockData)}</h3>
+			<h3 className="text-xl font-bold text-ellipsis">
+				{loading ? "loading..." : formatDollars(data!.allAccValue)}
+			</h3>
 			<p className="text-xs">As of {formatDate(date.toLocaleDateString())}</p>
 		</div>
 	);
