1098125a3a4ef78d800233d0ddffb470a2ae0575
LV <linvu2@publicisgroupe.net>
2023-07-27 10:18:04 -0400

    deleted my function

--- src/utilities/reactiveVariables.ts
+++ src/utilities/reactiveVariables.ts
@@ -7,21 +7,4 @@ export const sidebarClosed = makeVarPersisted("sidebarClosed", false);
 export const currentAccountId = makeVarPersisted("currentAccountId", 0);
 export const userInfo = makeVarPersisted<User | null>("user", null);
 
-type DateRanges = {
-	two: string[];
-	full: string[];
-	half: string[];
-};
-
-export const dateRanges: DateRanges = {
-	two: getMostRecentMonths(24),
-	full: getMostRecentMonths(12),
-	half: getMostRecentMonths(6),
-};
-
-export const lineChartDateRange = makeVarPersisted<string[]>(
-	"lineChartDateRange",
-	dateRanges.full
-);
-
 export const symbol = makeVar("");
