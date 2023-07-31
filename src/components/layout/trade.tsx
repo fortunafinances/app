13fa7351067c9a306dd9a33a334e19ef5067ff2a
lindsayliu <rll46@duke.edu>
2023-07-31 09:09:48 -0400

    added success notifs

--- src/components/layout/trade.tsx
+++ src/components/layout/trade.tsx
@@ -8,83 +8,89 @@ import AutoSizer, { Size } from "react-virtualized-auto-sizer";
 import TransferSuccessful from "../popup/transferSuccessful";
 
 export interface TradeProps {
-  buyState: React.Dispatch<React.SetStateAction<boolean>>;
+	buyState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 type State = {
-  tradeType: boolean;
+	tradeType: boolean;
 };
 
 export default function Trade() {
-  const location = useLocation();
-  const state: State = location.state as State;
-  //if true, BUY screen
-  //if false, SELL screen
-  const [buy, setBuy] = useState(true);
-  const [header, setHeader] = useState("BUY");
-  const [switchButton, setSwitchButton] = useState("SELL");
+	const location = useLocation();
+	const state: State = location.state as State;
+	//if true, BUY screen
+	//if false, SELL screen
+	const [buy, setBuy] = useState(true);
+	const [header, setHeader] = useState("BUY");
+	const [switchButton, setSwitchButton] = useState("SELL");
 
-  useEffect(() => {
-    if (buy === false) {
-      setHeader("SELL");
-      setSwitchButton("BUY");
-    } else {
-      setHeader("BUY");
-      setSwitchButton("SELL");
-    }
-  }, [buy]);
+	useEffect(() => {
+		if (buy === false) {
+			setHeader("SELL");
+			setSwitchButton("BUY");
+		} else {
+			setHeader("BUY");
+			setSwitchButton("SELL");
+		}
+	}, [buy]);
 
-  useEffect(() => {
-    if (state !== null && state !== undefined) {
-      const { tradeType } = state;
-      setBuy(tradeType);
-    }
-  }, [state]);
+	useEffect(() => {
+		if (state !== null && state !== undefined) {
+			const { tradeType } = state;
+			setBuy(tradeType);
+		}
+	}, [state]);
 
-  return (
-    <AutoSizer>
-      {({ height, width }: Size) => (
-        <div style={{ height, width }} className="overflow-y-auto">
-          <TransferSuccessful transfer={false} modalId="trade_successful" />
-          <div className="h-full flex">
-            <div
-              className={twMerge("bg-[#F9E5E5] w-[35%]", buy && "bg-success")}
-            >
-              <div className="relative mt-5">
-                <div
-                  className={twMerge(
-                    "text-[#920000] font-semibold ml-5",
-                    buy && "text-success-content"
-                  )}
-                >
-                  <h1 className="text-4xl">{header}</h1>
-                  <p className="text-xl">Account Name</p>
-                  <button
-                    className="absolute top-0 right-10 text-xl flex flex-row items-center"
-                    onClick={() => {
-                      setBuy((buy) => !buy);
-                    }}
-                  >
-                    <HiSwitchHorizontal color="black" />
-                    <p
-                      className={twMerge(
-                        "text-success-content font-medium",
-                        buy && "text-[#920000]"
-                      )}
-                    >
-                      {switchButton}
-                    </p>
-                  </button>
-                </div>
-                <TradeForm buyOrSell={buy} />
-              </div>
-            </div>
-            <div className="flex-1 flex ">
-              <StockInfo />
-            </div>
-          </div>
-        </div>
-      )}
-    </AutoSizer>
-  );
+	return (
+		<AutoSizer>
+			{({ height, width }: Size) => (
+				<div style={{ height, width }} className="overflow-y-auto">
+					<TransferSuccessful
+						transfer={false}
+						modalId="trade_successful"
+					/>
+					<div className="h-full flex">
+						<div
+							className={twMerge(
+								"bg-[#F9E5E5] w-[35%]",
+								buy && "bg-success",
+							)}
+						>
+							<div className="relative mt-5">
+								<div
+									className={twMerge(
+										"text-[#920000] font-semibold ml-5",
+										buy && "text-success-content",
+									)}
+								>
+									<h1 className="text-4xl">{header}</h1>
+									<p className="text-xl">Account Name</p>
+									<button
+										className="absolute top-0 right-10 text-xl flex flex-row items-center"
+										onClick={() => {
+											setBuy((buy) => !buy);
+										}}
+									>
+										<HiSwitchHorizontal color="black" />
+										<p
+											className={twMerge(
+												"text-success-content font-medium",
+												buy && "text-[#920000]",
+											)}
+										>
+											{switchButton}
+										</p>
+									</button>
+								</div>
+								<TradeForm buyOrSell={buy} />
+							</div>
+						</div>
+						<div className="flex-1 flex ">
+							<StockInfo />
+						</div>
+					</div>
+				</div>
+			)}
+		</AutoSizer>
+	);
 }
