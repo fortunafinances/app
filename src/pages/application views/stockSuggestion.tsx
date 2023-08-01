f8fe3aa767cb2507265a084248faed1d6bfeaef9
LV <linvu2@publicisgroupe.net>
2023-08-01 11:09:08 -0400

    stock selection store 5 stocks

--- src/pages/application views/stockSuggestion.tsx
+++ src/pages/application views/stockSuggestion.tsx
@@ -7,6 +7,7 @@ import {
     currentAccountId,
     userInfo,
 } from "../../utilities/reactiveVariables";
+import { useState } from "react";
 
 
 export default function StockSuggestion() {
@@ -28,10 +29,36 @@ export default function StockSuggestion() {
         "Blockchain",
         "Metaverse",
         "AI"];
+    const [selections, setSelections] = useState<string[]>([]);
     const navigate = useNavigate();
     const user = useReactiveVar(userInfo);
 
+    const btnSelection = (category: string) => {
+        if (selections.length < 5 && !selections.includes(category)) {
+            setSelections([...selections, category]);
+        }
+        if (selections.includes(category)) {
+            const updatedSelections = selections.filter((item) => item !== category);
+            setSelections(updatedSelections);
+        }
+        console.log("selections: ", selections);
+    }
 
+    const isBtnSelected = (category: string) => {
+        return selections.length > 0 && selections.includes(category);
+    }
+
+    // create buttons to display
+    const SuggestionButton = ({ text, ...props }) => {
+        return (
+            <button
+                className={`focus:bg-[#2a0066] focus:text-gray-50 flex-1 px-5 py-2.5 relative group overflow-hidden font-medium bg-transparent-50 text-gray-600 border border-[#2a0066] hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 inline-block rounded m-2 
+                            ${isBtnSelected(text) ? 'bg-[#2a0066] text-gray-50' : ''}`}{...props}
+            >
+                {text}
+            </button>
+        );
+    };
 
     return (
         <div className="h-screen flex [&>div]:w-[50%]">
@@ -39,7 +66,6 @@ export default function StockSuggestion() {
                 <h1 className=" mt-[30%] font-semibold text-left md:text-7xl text-5xl">
                     Let's invest in your first stock
                 </h1>
-
             </div>
             <div className="bg-accent p-4 text-primary">
                 <h1 className="text-1xl md:text-3xl">Select 5 categories that you're most interested in</h1>
@@ -47,7 +73,12 @@ export default function StockSuggestion() {
                 <div className="App">
                     <center>
                         {categories.map((item, index) => (
-                            <SuggestionButton key={index} text={item} onClick={() => handleButtonClick(item)} />
+                            <SuggestionButton
+                                key={index}
+                                text={item}
+                                onClick={() => btnSelection(item)}
+                                isSelected={isBtnSelected(item)}
+                            />
                         ))}
 
                     </center>
@@ -57,19 +88,8 @@ export default function StockSuggestion() {
     );
 }
 
-function handleButtonClick(item) {
 
-}
 
-const SuggestionButton = ({ text, ...props }) => {
-    return (
-        <button
-            className="focus:bg-[#2a0066] focus:text-gray-50 flex-1 px-5 py-2.5 relative group overflow-hidden font-medium bg-transparent-50 text-gray-600 border  border-[#2a0066] hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 inline-block rounded m-2"
-            {...props}
-        >
-            {text}
-        </button>
-    );
-};
+
 
 
