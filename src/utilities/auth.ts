2b033b69c3464915a569964e5e109f1a26156d88
JR <johnryan.byers@publicissapient.com>
2023-08-02 15:56:08 -0400

    additional code for deploy

--- src/utilities/auth.ts
+++ src/utilities/auth.ts
@@ -19,7 +19,7 @@ const auth0Client = new auth0.WebAuth({
 	domain: "dev-wpc8kymxzmepqxl5.us.auth0.com",
 	clientID: "OxQxuofsPZXSFzTqbVtKgErT2xrl3VfZ",
 	audience: "http://127.0.0.1:5000/",
-	redirectUri: "http://localhost:4040/callback",
+	redirectUri: import.meta.env.DEV ? "http://localhost:4040/callback" : "https://cvppf9v39y.us-east-1.awsapprunner.com/callback",
 	responseType: "token id_token",
 	scope: "openid email sub nickname profile read:user", // what we want the token to include
 });
@@ -39,7 +39,7 @@ export function signout() {
 	localStorage.removeItem("accounts");
 	localStorage.removeItem("currentAccountId");
 	auth0Client.logout({
-		returnTo: "http://localhost:4040/",
+		returnTo: import.meta.env.DEV ? "http://localhost:4040/" : "https://cvppf9v39y.us-east-1.awsapprunner.com/",
 		clientID: "OxQxuofsPZXSFzTqbVtKgErT2xrl3VfZ",
 	});
 }
