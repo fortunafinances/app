463e2fa1cee52ea832a2bcfe8f969bfe93cca19b
Leila Baniassad <lbaniassad@gmail.com>
2023-08-03 11:11:09 -0400

    updated order and holdings cards

--- src/components/data/cards/holdingCard.tsx
+++ src/components/data/cards/holdingCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { formatDollars } from "../../../utilities/currency";
+// import {changePercent} from "../stockInfo"
 
 interface CardProps {
 	ticker: string;
@@ -17,13 +18,14 @@ const CardComponent: React.FC<CardProps> = ({
 	<div className="card card-bordered bg-base-100 selection:shadow-xl">
 		<div className="flex flex-row justify-between card-body">
 			<div>
-				{" "}
-				<h2 className="card-title">{ticker}</h2>
-				<p>{company}</p>
+			<button className="btn text-4xl drop-shadow-md bg-base-100">{ticker}</button>
+				{/* <h2 className="card-title text-4xl">{ticker}</h2> */}
+				<p className="text-gray-400 font-bold">{company}</p>
+				<h2 className="text-sm">{tradeQty} Shares</h2>
 			</div>
 			<div className="">
-				<h2 className="text-2xl">{formatDollars(tradeQty * tradePrice)}</h2>
-                <p>{tradeQty} x {formatDollars(tradePrice)}</p>
+                <h2 className="text-3xl">{formatDollars(tradePrice)}</h2>
+				{/* <p>{changePercent}</p> */}
 			</div>
 		</div>
 	</div>
