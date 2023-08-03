b29102dce505acd8c16b628eea8bc3da79739544
Nolan Gelinas <nolangelinas@gmail.com>
2023-08-03 15:10:42 -0400

    Default sidebar closed on mobile

--- src/utilities/reactiveVariables.ts
+++ src/utilities/reactiveVariables.ts
@@ -1,7 +1,10 @@
 import { makeVarPersisted } from "./hooks";
 import { User } from "./types";
 
-export const sidebarClosed = makeVarPersisted("sidebarClosed", false);
+export const sidebarClosed = makeVarPersisted(
+	"sidebarClosed",
+	window.innerWidth < 640 ? true : false,
+);
 export const currentAccountId = makeVarPersisted("currentAccountId", 0);
 export const userInfo = makeVarPersisted<User | null>("user", null);
 
