0d8f68ca3701e56dbb7c226b4059f4f7f3c1e606
Nolan Gelinas <nolangelinas@gmail.com>
2023-08-03 12:42:55 -0400

    Fix dev

--- src/components/data/cards/holdingCard.tsx
+++ src/components/data/cards/holdingCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatDollars } from "../../../utilities/currency";
+import { formatDollars, percentChange } from "../../../utilities/common";
 import { useNavigate } from "react-router-dom";
 import { symbol } from "../../../utilities/reactiveVariables";
 
@@ -11,38 +11,38 @@ interface CardProps {
 	prevPrice: number;
 }
 
-
 const CardComponent: React.FC<CardProps> = ({
 	ticker,
 	company,
 	tradeQty,
 	tradePrice,
-	prevPrice
+	prevPrice,
 }) => {
-const navigate = useNavigate()
-const dataPoints = [
-	{ x: 'prev', y: prevPrice },
-	{ x: 'curr', y: tradePrice },
-  ];
+	const navigate = useNavigate();
 	return (
-	<div className="card card-bordered bg-base-100 selection:shadow-xl">
-		<div className="flex flex-row justify-between card-body">
-			<div>
-			<button
-			onClick={()=>{
-				symbol(ticker);
-				navigate("/app/trade")
-			}} className="btn text-4xl drop-shadow-md bg-base-100">{ticker}</button>
-				{/* <h2 className="card-title text-4xl">{ticker}</h2> */}
-				<p className="text-gray-400 font-bold">{company}</p>
-				<h2 className="text-sm">{tradeQty} Shares</h2>
-			</div>
-			<div className="">
-                <h2 className="text-3xl">{formatDollars(tradePrice)}</h2>
-				{/* <p>{convertToRoundedPercentageChange(dataPoints)}</p> */}
+		<div className="card card-bordered bg-base-100 selection:shadow-xl">
+			<div className="flex flex-row justify-between card-body">
+				<div>
+					<button
+						onClick={() => {
+							symbol(ticker);
+							navigate("/app/trade");
+						}}
+						className="btn text-4xl drop-shadow-md bg-base-100"
+					>
+						{ticker}
+					</button>
+					{/* <h2 className="card-title text-4xl">{ticker}</h2> */}
+					<p className="text-gray-400 font-bold">{company}</p>
+					<h2 className="text-sm">{tradeQty} Shares</h2>
+				</div>
+				<div className="">
+					<h2 className="text-3xl">{formatDollars(tradePrice)}</h2>
+					<p>{percentChange(tradePrice, prevPrice)}</p>
+				</div>
 			</div>
 		</div>
-	</div>
-)};
+	);
+};
 
 export default CardComponent;
