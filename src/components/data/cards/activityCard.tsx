3888c56f45e3818aa00fe534f0252009ac2bd9bd
Nolan Gelinas <nolangelinas@gmail.com>
2023-08-07 17:05:34 -0400

    Fix font

--- src/components/data/cards/activityCard.tsx
+++ src/components/data/cards/activityCard.tsx
@@ -15,19 +15,18 @@ const ActivityCard: React.FC<ActivityProps> = ({
 	description,
 	amount,
 }) => (
-	<div>
-
-		<div className="card card-bordered bg-base-100 selection:shadow-xl">
-			<div className="flex flex-row justify-between card-body">
-				<div>
-					{" "}
-					<h2 className="card-title text-3xl">{type}</h2>
-					<p className="text-gray-500 text-sm">{description}</p>
-				</div>
-				<div className="flex flex-col items-end">
-					<h2 className="text-3xl">{formatDollars(amount)}</h2>
-					<p className= "text-xs py-3 whitespace-nowrap">{formatDate(date)}</p>
-				</div>
+	<div className="card card-bordered bg-base-100 selection:shadow-xl">
+		<div className="flex flex-row justify-between card-body">
+			<div>
+				{" "}
+				<h2 className="card-title text-3xl">{type}</h2>
+				<p className="text-gray-500 text-sm">{description}</p>
+			</div>
+			<div className="flex flex-col items-end">
+				<h2 className="text-2xl">{formatDollars(amount)}</h2>
+				<p className="text-xs py-3 whitespace-nowrap">
+					{formatDate(date)}
+				</p>
 			</div>
 		</div>
 	</div>
