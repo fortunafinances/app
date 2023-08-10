3ca9f552af5bdae41ba6081b83f38afd9e23e14a
Linh Vu <linhvu.138@utexas.edu>
2023-08-10 10:56:19 -0400

    edited auth

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,45 +1,13 @@
 import { useEffect, useState } from "react";
 import { handleAuthentication } from "../utilities/auth";
-import { gql, useLazyQuery, useMutation, useReactiveVar } from "@apollo/client";
-import { Account, User } from "../utilities/types";
+import { useReactiveVar } from "@apollo/client";
 import { currentAccountId, userInfo } from "../utilities/reactiveVariables";
-import { GET_ACCOUNTS } from "../utilities/graphQL";
 import { useNavigate } from "react-router-dom";
 
-const GET_USER = gql`
-	mutation InsertUser($userId: ID!, $email: String!) {
-		insertUser(userId: $userId, email: $email) {
-			message
-			user {
-				userId
-				onboardingComplete
-				username
-				firstName
-				lastName
-				email
-				phoneNumber
-				picture
-			}
-		}
-	}
-`;
-
-type UserQuery = {
-	insertUser: {
-		user: User;
-	};
-};
-
-type AccountQuery = {
-	accounts: Account[];
-};
-
 const Callback = () => {
-	const [getAccounts] = useLazyQuery<AccountQuery>(GET_ACCOUNTS);
 	const navigate = useNavigate();
-	const [getUser] = useMutation<UserQuery>(GET_USER);
 	const [connectionError, setConnectionError] = useState(false);
-
+	const accId = useReactiveVar(currentAccountId);
 	const user = useReactiveVar(userInfo);
 
 	useEffect(() => {
@@ -52,59 +20,14 @@ const Callback = () => {
 		}, 10000);
 
 		setTimeout(() => {
-			// Get user data
-			getUser({
-				variables: {
-					userId: user!.userId,
-					email: user!.email,
-				},
-			})
-				.then((res) => {
-					switch (res.data?.insertUser.user.onboardingComplete) {
-						case 0: {
-							navigate("/createProfile", { replace: true });
-							break;
-						}
-						case 1: {
-							userInfo({
-								username: res.data?.insertUser.user.username,
-								firstName: res.data?.insertUser.user.firstName,
-								lastName: res.data?.insertUser.user.lastName,
-								phoneNumber:
-									res.data?.insertUser.user.phoneNumber,
-								...user!,
-							});
-							navigate("/createAccount", { replace: true });
-							break;
-						}
-						default: {
-							// If user has completed onboarding, navigate to dashboard and populate user info
-							userInfo({
-								username: res.data?.insertUser.user.username,
-								firstName: res.data?.insertUser.user.firstName,
-								lastName: res.data?.insertUser.user.lastName,
-								phoneNumber:
-									res.data?.insertUser.user.phoneNumber,
-								...user!,
-							});
-							getAccounts({ variables: { userId: user!.userId } })
-								.then((res) => {
-									currentAccountId(
-										res.data?.accounts[0].accId,
-									);
-								})
-								.catch((err) => {
-									console.error(err);
-								});
-							navigate("/app", { replace: true });
-						}
-					}
-				})
-				.catch((err) => {
-					console.error(err);
-				});
+			if (accId == 0) {
+				navigate("/createProfile", { replace: true });
+			} else if (accId == 1) {
+				navigate("/app", { replace: true });
+			}
+
 		}, 2000);
-	}, [getAccounts, getUser, navigate, user]);
+	}, [accId, navigate, user]);
 
 	return (
 		<div className="w-screen h-screen">
--- src/utilities/auth.ts
+++ src/utilities/auth.ts
@@ -1,12 +1,12 @@
 import auth0 from 'auth0-js';
-import { userInfo } from "./reactiveVariables";
+import { currentAccountId, userInfo } from "./reactiveVariables";
 
 // WebAuth will redirect user to the login page
 const auth0Client = new auth0.WebAuth({
 	// feed in the data
 	// https://dev-wpc8kymxzmepqxl5.us.auth0.com/api/v2/
-	domain: "dev-wpc8kymxzmepqxl5.us.auth0.com",
-	clientID: "OxQxuofsPZXSFzTqbVtKgErT2xrl3VfZ",
+	domain: "dev-x4clr8ssigb1k3ok.us.auth0.com",
+	clientID: "8GfA02pHpCVxqZC4qXPS8sycPhyZtjAB",
 	audience: "http://127.0.0.1:5000/",
 	redirectUri: import.meta.env.DEV ? "http://localhost:4040/callback" : "https://fortunafinances.com/callback",
 	responseType: "token id_token",
@@ -14,10 +14,12 @@ const auth0Client = new auth0.WebAuth({
 });
 
 export function login() {
+	currentAccountId(1);
 	auth0Client.authorize();
 }
 
 export function signup() {
+	currentAccountId(0);
 	auth0Client.authorize({ screen_hint: "signup" });
 }
 
@@ -30,7 +32,7 @@ export function signout() {
 	localStorage.removeItem("symbol");
 	auth0Client.logout({
 		returnTo: import.meta.env.DEV ? "http://localhost:4040/" : "https://fortunafinances.com/",
-		clientID: "OxQxuofsPZXSFzTqbVtKgErT2xrl3VfZ",
+		clientID: "8GfA02pHpCVxqZC4qXPS8sycPhyZtjAB",
 	});
 }
 
